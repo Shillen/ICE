8ad04db1f94405d00136022387a3b72c66a974f0
Shillen <joey@mitsn.nl>
2016-03-11 11:13:30 +0100

    New project
    New project created for asp.net 4.5 instead of 5

---
+++ ICE-Server/ICE-Server/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ICE_Server.Startup))]
+namespace ICE_Server
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
