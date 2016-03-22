3019e25de4304477c5008992ca959e8f8f3faa12
Shillen <joey@mitsn.nl>
2016-03-22 14:35:02 +0100

    predefmessages

--- ICE-Server/ICE-Server/PushMessages.cs
+++ ICE-Server/ICE-Server/PushMessages.cs
@@ -14,13 +14,25 @@ namespace ICE_Server
     public class PushMessages
     {
         private PushBroker pushBroker { get; set; }
-        private string pushMessage { get; set; }
+        private string message { get; set; }
         private bool succces = true;
 
         /// <param name="message">String of the message</param>
-        public Pushmessage(string pushMessage)
+        public Pushmessage(string message)
         {
+            this.message = message;
             this.pushBroker = new PushBroker();
+            
+            // Regiser all the events
+            this.pushBroker.OnChannelCreated += push_OnChannelCreated;
+            this.pushBroker.OnChannelDestroyed += push_OnChannelDestroyed;
+            this.pushBroker.OnChannelException += push_OnChannelException;
+            this.pushBroker.OnNotificationRequeue += push_OnNotificationRequeue;
+            this.pushBroker.OnServiceException += push_OnServiceException;
+            this.pushBroker.OnNotificationSent += push_OnNotificationSent;
+            this.pushBroker.OnNotificationFailed += push_OnNotificationFailed;
+            this.pushBroker.OnDeviceSubscriptionExpired += push_OnDeviceSubscriptionExpired;
+
             this.pushBroker.RegisterGcmService(new GcmPushChannelSettings("AIzaSyBLIvuek1e_DKEk0wnXddj3RtkSUO8ZluY"));
         }
     }
