df99673a7d2688ef3ab52dc346f273f74acb0cc1
Michel van der hoorn <mvan_der_hoorn@hotmail.com>
2016-04-13 12:32:10 +0200

    Created a synchronize method in SynchronizeController for synchronizing all the types of tables

--- ICE-Server/ICE-Server/Controllers/SynchronizeController.cs
+++ ICE-Server/ICE-Server/Controllers/SynchronizeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ICE_Server.DAL;
+using ICE_Server.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,6 +11,55 @@ namespace ICE_Server.Controllers
 {
     public class SynchronizeController : ApiController
     {
+        private BroadcastsRepository broadcastRepository;
+        private BuildingsRepository buildingRepository;
+        private DevicesRepository deviceRepository;
+        private EmergencyRepository emergencyRepository;
+        private EmergencyTranslatedRepository emergencytranslatedRepository;
+        private LanguagesRepository languageRepository;
+        private PredefinedMessagesRepository predefinedmessageRepository;
+
+        public SynchronizeController()
+        {
+            this.broadcastRepository = new BroadcastsRepository(new ICEContext());
+            this.buildingRepository = new BuildingsRepository(new ICEContext());
+            this.deviceRepository = new DevicesRepository(new ICEContext());
+            this.emergencyRepository = new EmergencyRepository(new ICEContext());
+            //this.emergencytranslatedRepository = new EmergencyTranslatedRepository(new ICEContext());
+            this.predefinedmessageRepository = new PredefinedMessagesRepository(new ICEContext());
+        }
+        // GET api/synchronize
+        //[ResponseType(typeof(IEnumerable<Activity>))]
+        [Route("api/synchronize/{tableType}")]
+        [HttpGet]
+        public IHttpActionResult Synchronize(string tableType)
+        {
+            if(tableType.Equals("Broadcasts"))
+            {
+                return Ok(broadcastRepository.GetAll());
+            }
+            if(tableType.Equals("Buildings"))
+            {
+                return Ok(buildingRepository.GetAll());
+            }
+            if(tableType.Equals("Devices"))
+            {
+                return Ok(deviceRepository.GetAll());
+            }
+            if(tableType.Equals("Emergencies"))
+            {
+                return Ok(emergencyRepository.GetAll());
+            }
+            if(tableType.Equals("PredefinedMessages"))
+            {
+                return Ok(predefinedmessageRepository.GetAll());
+            }
+            else
+            {
+                return NotFound();
+            }
+            //return Mapper.DynamicMap<IEnumerable<DboActivity>, IEnumerable<Activity>>(activityRepository.SearchTodaysActivities(query));
+        }
         // SYNCHRONIIIIIIIIIIZE
     }
 }
